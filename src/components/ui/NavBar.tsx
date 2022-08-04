d1c8a58f36b8992681e7837fa8221c120c7a7825
Jacob Gad <jacob.gad@hotmail.com>
2022-08-04 19:56:11 +1000

    fix: :bug: Fix signout redirect bug

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -2,9 +2,12 @@ import { HomeIcon } from '@heroicons/react/solid';
 import { signOut, signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import toast from 'react-hot-toast';
 
 export default function NavBar() {
 	const { data: session } = useSession();
+	const router = useRouter();
 
 	return (
 		<nav className='flex items-center justify-between bg-slate-800 py-2 px-4'>
@@ -27,15 +30,23 @@ export default function NavBar() {
 			)}
 			{session ? (
 				<button
-					onClick={() => signOut()}
-					className='rounded px-2 py-1 hover:bg-slate-400 hover:text-black'
+					onClick={() => router.push('/').then(() => signOut())}
+					className='hover:text-bg-slate-800 rounded px-3 py-2 ring-1 ring-slate-400 
+					hover:bg-slate-400 hover:bg-opacity-25'
 				>
 					Sign Out
 				</button>
 			) : (
 				<button
-					onClick={() => signIn()}
-					className='rounded bg-sky-600 px-2 py-1 hover:bg-sky-400 hover:text-black'
+					onClick={() => {
+						const promise = signIn('google');
+						toast.promise(promise, {
+							loading: 'Checking with google...',
+							success: 'Welcome',
+							error: 'Oops there was an error',
+						});
+					}}
+					className='rounded bg-sky-600 px-5 py-2 hover:bg-sky-400 hover:text-black'
 				>
 					Sign In
 				</button>
