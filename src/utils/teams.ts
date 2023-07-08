acad3604000847a33b3920db4d0eb310e6cb1137
Jacob Gad <jacobgad5262@gmail.com>
2023-07-08 17:10:10 +1000

    feat: :sparkles: add rgb values to team data

--- src/utils/teams.ts
+++ src/utils/teams.ts
@@ -1,5 +1,6 @@
 export interface TeamOption {
 	name: string;
+	rgb: { r: number; g: number; b: number };
 	color: {
 		main: string;
 		dark: string;
@@ -10,6 +11,7 @@ export interface TeamOption {
 export const teamOptions: TeamOption[] = [
 	{
 		name: 'Slate',
+		rgb: { r: 71, g: 85, b: 105 },
 		color: {
 			main: 'bg-slate-600',
 			dark: 'bg-slate-700',
@@ -18,6 +20,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Zinc',
+		rgb: { r: 82, g: 82, b: 91 },
 		color: {
 			main: 'bg-zinc-600',
 			dark: 'bg-zinc-700',
@@ -26,6 +29,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Stone',
+		rgb: { r: 87, g: 83, b: 78 },
 		color: {
 			main: 'bg-stone-600',
 			dark: 'bg-stone-700',
@@ -34,6 +38,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Red',
+		rgb: { r: 220, g: 38, b: 38 },
 		color: {
 			main: 'bg-red-600',
 			dark: 'bg-red-700',
@@ -42,6 +47,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Orange',
+		rgb: { r: 234, g: 88, b: 12 },
 		color: {
 			main: 'bg-orange-600',
 			dark: 'bg-orange-700',
@@ -50,6 +56,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Amber',
+		rgb: { r: 217, g: 119, b: 6 },
 		color: {
 			main: 'bg-amber-600',
 			dark: 'bg-amber-700',
@@ -58,6 +65,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Yellow',
+		rgb: { r: 202, g: 138, b: 4 },
 		color: {
 			main: 'bg-yellow-600',
 			dark: 'bg-yellow-700',
@@ -66,6 +74,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Lime',
+		rgb: { r: 101, g: 163, b: 13 },
 		color: {
 			main: 'bg-lime-600',
 			dark: 'bg-lime-700',
@@ -74,6 +83,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Green',
+		rgb: { r: 22, g: 163, b: 74 },
 		color: {
 			main: 'bg-green-600',
 			dark: 'bg-green-700',
@@ -82,6 +92,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Emerald',
+		rgb: { r: 5, g: 150, b: 105 },
 		color: {
 			main: 'bg-emerald-600',
 			dark: 'bg-emerald-700',
@@ -90,6 +101,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Teal',
+		rgb: { r: 13, g: 148, b: 136 },
 		color: {
 			main: 'bg-teal-600',
 			dark: 'bg-teal-700',
@@ -98,6 +110,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Cyan',
+		rgb: { r: 8, g: 145, b: 178 },
 		color: {
 			main: 'bg-cyan-600',
 			dark: 'bg-cyan-700',
@@ -106,6 +119,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Sky',
+		rgb: { r: 2, g: 132, b: 199 },
 		color: {
 			main: 'bg-sky-600',
 			dark: 'bg-sky-700',
@@ -114,6 +128,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Blue',
+		rgb: { r: 37, g: 99, b: 235 },
 		color: {
 			main: 'bg-blue-600',
 			dark: 'bg-blue-700',
@@ -122,6 +137,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Indigo',
+		rgb: { r: 79, g: 70, b: 229 },
 		color: {
 			main: 'bg-indigo-600',
 			dark: 'bg-indigo-700',
@@ -130,6 +146,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Violet',
+		rgb: { r: 124, g: 58, b: 237 },
 		color: {
 			main: 'bg-violet-600',
 			dark: 'bg-violet-700',
@@ -138,6 +155,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Purple',
+		rgb: { r: 147, g: 51, b: 234 },
 		color: {
 			main: 'bg-purple-600',
 			dark: 'bg-purple-700',
@@ -146,6 +164,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Fuchsia',
+		rgb: { r: 192, g: 38, b: 211 },
 		color: {
 			main: 'bg-fuchsia-600',
 			dark: 'bg-fuchsia-700',
@@ -154,6 +173,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Pink',
+		rgb: { r: 219, g: 39, b: 119 },
 		color: {
 			main: 'bg-pink-600',
 			dark: 'bg-pink-700',
@@ -162,6 +182,7 @@ export const teamOptions: TeamOption[] = [
 	},
 	{
 		name: 'Rose',
+		rgb: { r: 255, g: 29, b: 72 },
 		color: {
 			main: 'bg-rose-600',
 			dark: 'bg-rose-700',
